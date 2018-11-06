a708e8265d79d0a565c312d8b9ff788a2dc8ff2b
Kyle Peeler <kylepeeler@gmail.com>
2018-11-06 00:48:55 -0500

    abstracted out enzyme setup, added check for _ps global

--- tests/PSClickWrap.test.js
+++ tests/PSClickWrap.test.js
@@ -1,29 +1,15 @@
 import React from 'react'
-import {configure, mount} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter() });
-import {render, unmountComponentAtNode} from 'react-dom'
-
+import {mount} from 'enzyme';
 import {PSClickWrap} from '../src';
 
 describe('PSClickWrap', () => {
-  // let node
 
-  // beforeEach(() => {
-  //   node = document.createElement('div')
-  // })
-
-  // afterEach(() => {
-  //   unmountComponentAtNode(node)
-  // })
-
-  it('Renders a ps-group div', () => {
+  it('Renders a ps-clickwrap div', () => {
     let wrapper = mount(<PSClickWrap accessId={"29ea80d9-d386-4cfd-a280-505e802ee732"} signerIDSelector={"email"} groupKey={"example-clickwrap"} displayAll={true} testMode={true} />);
-    expect(wrapper.containsMatchingElement(<div class="ps-contract"></div>));
+    expect(wrapper.containsMatchingElement(<div class="ps-clickwrap"></div>));
   })
 
-  it('Renders a contract body', () => {
-    let wrapper = mount(<PSClickWrap accessId={"29ea80d9-d386-4cfd-a280-505e802ee732"} signerIDSelector={"email"} groupKey={"example-clickwrap"} displayAll={true} testMode={true} />);
-    expect(wrapper.containsMatchingElement(<div class="ps-contract-body"></div>));
+  it('Creates _ps global', () => {
+    expect(_ps).toBeDefined();
   })
 })
