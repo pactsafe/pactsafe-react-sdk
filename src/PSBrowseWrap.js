df8e06988a35abaa63948c0b279349dd81990b2a
Kyle Peeler <kylepeeler@gmail.com>
2018-11-05 22:48:57 -0500

    added jest

--- src/PSBrowseWrap.js
+++ src/PSBrowseWrap.js
@@ -1,89 +1,104 @@
 /* global _ps */
-import React from 'react';
-import PropTypes from 'prop-types';
-import isRequiredIf from 'react-proptype-conditional-require';
+import React from "react";
+import PropTypes from "prop-types";
+import isRequiredIf from "react-proptype-conditional-require";
 
-class PSBrowseWrap extends React.Component{
-    constructor(props){
-        super(props);
-        const PSUrl = this.props.psScriptURL;
-        if (!this.isSnippetLoaded(PSUrl)){
-            (function (window, document, script, src, pso, a, m) {
-                window['PactSafeObject'] = pso;
-                window[pso] = window[pso] || function () {
-                    (window[pso].q = window[pso].q || []).push(arguments);
-                }, window[pso].on = function () {
-                    (window[pso].e = window[pso].e || []).push(arguments);
-                }, window[pso].once = function () {
-                    (window[pso].eo = window[pso].eo || []).push(arguments);
-                }, window[pso].off = function () {
-                    (window[pso].o = window[pso].o || []).push(arguments);
-                }, window[pso].t = 1 * new Date();
-                a = document.createElement(script),
-                m = document.getElementsByTagName(script)[0];
-                a.async = 1;
-                a.src = src;
-                m.parentNode.insertBefore(a, m);
-                // window[pso].debug = true;
-            })(window, document, 'script', PSUrl, '_ps');
-        }
-        this.targetSelector = 'psbw-' + this.props.groupKey;
-    }
+class PSBrowseWrap extends React.Component {
+	constructor(props) {
+		super(props);
+		const PSUrl = this.props.psScriptURL;
+		if (!this.isSnippetLoaded(PSUrl)) {
+			(function(window, document, script, src, pso, a, m) {
+				window["PactSafeObject"] = pso;
+				(window[pso] =
+					window[pso] ||
+					function() {
+						(window[pso].q = window[pso].q || []).push(arguments);
+					}),
+					(window[pso].on = function() {
+						(window[pso].e = window[pso].e || []).push(arguments);
+					}),
+					(window[pso].once = function() {
+						(window[pso].eo = window[pso].eo || []).push(arguments);
+					}),
+					(window[pso].off = function() {
+						(window[pso].o = window[pso].o || []).push(arguments);
+					}),
+					(window[pso].t = 1 * new Date());
+				(a = document.createElement(script)),
+					(m = document.getElementsByTagName(script)[0]);
+				a.async = 1;
+				a.src = src;
+				m.parentNode.insertBefore(a, m);
+				// window[pso].debug = true;
+			})(window, document, "script", PSUrl, "_ps");
+		}
+		this.targetSelector = "psbw-" + this.props.groupKey;
+	}
 
-    isSnippetLoaded(PSUrl){
-        if (!PSUrl){
-            PSUrl = this.props.psScriptURL;
-        }
-        const scripts = document.getElementsByTagName('script');
-        for (let i = 0; i < scripts.length; i++){
-            if (scripts[i].src === PSUrl) return true;
-        }
-        return false;
-    }
+	isSnippetLoaded(PSUrl) {
+		if (!PSUrl) {
+			PSUrl = this.props.psScriptURL;
+		}
+		const scripts = document.getElementsByTagName("script");
+		for (let i = 0; i < scripts.length; i++) {
+			if (scripts[i].src === PSUrl) return true;
+		}
+		return false;
+	}
 
-    componentWillMount(){
-        _ps('create', this.props.accessId);
-    }
+	componentWillMount() {
+		_ps("create", this.props.accessId);
+	}
 
-    componentDidMount(){
-        _ps('load', this.props.groupKey, {
-            target_selector: this.targetSelector,
-            position: this.props.position,
-            badge_text: this.props.badgeText,
-            always_visible: this.props.alwaysVisible,
-            open_legal_center: this.props.openLegalCenter
-        });
-    }
+	componentDidMount() {
+		_ps("load", this.props.groupKey, {
+			target_selector: this.targetSelector,
+			position: this.props.position,
+			badge_text: this.props.badgeText,
+			always_visible: this.props.alwaysVisible,
+			open_legal_center: this.props.openLegalCenter
+		});
+	}
 
-    render(){
-        return(
-            <a href={this.props.link} id={this.targetSelector}>{this.props.linkText}</a>
-        );
-    }
+	render() {
+		return (
+			<a href={this.props.link} id={this.targetSelector}>
+				{this.props.linkText}
+			</a>
+		);
+	}
 
-    componentWillUnmount(){
-        _ps.getByKey(this.props.groupKey).rendered = false;
-    }
+	componentWillUnmount() {
+		_ps.getByKey(this.props.groupKey).rendered = false;
+	}
 }
 
-PSBrowseWrap.MUST_PROVIDE_LINK_IF_OPEN_LEGAL_CENTER_FALSE = 'PSBrowseWrap Error: You must provide a link prop if openLegalCenter is passed false';
+PSBrowseWrap.MUST_PROVIDE_LINK_IF_OPEN_LEGAL_CENTER_FALSE =
+	"PSBrowseWrap Error: You must provide a link prop if openLegalCenter is passed false";
 
 PSBrowseWrap.propTypes = {
 	accessId: PropTypes.string.isRequired,
 	alwaysVisible: PropTypes.bool,
 	badgeText: PropTypes.string,
 	groupKey: PropTypes.string.isRequired,
-	link: isRequiredIf(PropTypes.string, props => (props.hasOwnProperty('openLegalCenter') && props.openLegalCenter === false), PSBrowseWrap.MUST_PROVIDE_LINK_IF_OPEN_LEGAL_CENTER_FALSE),
+	link: isRequiredIf(
+		PropTypes.string,
+		props =>
+			props.hasOwnProperty("openLegalCenter") &&
+			props.openLegalCenter === false,
+		PSBrowseWrap.MUST_PROVIDE_LINK_IF_OPEN_LEGAL_CENTER_FALSE
+	),
 	linkText: PropTypes.string.isRequired,
 	openLegalCenter: PropTypes.bool,
-	position: PropTypes.oneOf(['middle','left', 'right', 'auto']).isRequired,
-	psScriptURL: PropTypes.string.isRequired,
+	position: PropTypes.oneOf(["middle", "left", "right", "auto"]).isRequired,
+	psScriptURL: PropTypes.string.isRequired
 };
 
 PSBrowseWrap.defaultProps = {
-    psScriptURL: '//vault.pactsafe.io/ps.min.js',
-    position: 'auto',
-	link: '#',
+	psScriptURL: "//vault.pactsafe.io/ps.min.js",
+	position: "auto",
+	link: "#",
 	openLegalCenter: true
 };
 
